dddb93061352433a6c838a1eecaf9b832741865e
Vinicius Candido Chaves <viniciuschaves1007@gmail.com>
2026-05-13 21:50:50 -0300

    Add files via upload

---
+++ introducao.js
@@ -0,0 +1,66 @@
+//strings
+//alert("Meu primeiro alerta")
+//alert("Meu segundo alerta")
+
+//operações matematicas
+//alert(60+7)
+//alert(70-10)
+//alert(33*2)
+//alert(139/2)
+
+//variaveis
+//let numeroA = 8
+//let numeroB = 8
+
+//alert (numeroA+numeroB)
+//alert (numeroA-numeroB)
+//alert (numeroA*numeroB)
+//alert (numeroA/numeroB)
+
+//let nome = " Vinicius Chaves"
+//let email = "viniciuschaves1007@gmail.com"
+//let cargo = "Desenvolvedor"
+//let unidade = "Senai cotia"
+//let idade = 15
+
+//alert("o nome do usuario é: " + nome)
+//alert("seu email é: " + email )
+//alert("seu cargo é: " + cargo)
+//alert("Sua Unidade é: " + unidade)
+//alert("Sua idade é " + idade + " anos")
+
+//let nome = prompt("Insira o nome")
+//let email = prompt("Insira seu email")
+//let cargo = prompt("Insira seu cargo")
+//let unidade = prompt("Insira sua unidade")
+//let idade = prompt("Insira sua idade ")
+
+//alert("o nome do usuario é: " + nome)
+//alert("seu email é: " + email )
+//alert("seu cargo é: " + cargo)
+//alert("Sua Unidade é: " + unidade)
+//lert("Sua idade é " + idade + " anos")
+
+// exercico 1
+
+let numeroA = prompt("insira um numero qualquer")
+let numeroB = prompt("Insira um numero qualquer")
+
+numeroA = parseInt(numeroA)
+numeroB = parseInt(numeroB)
+
+alert ("A soma: " + (numeroA+numeroB))
+alert ("a subtração: " + (numeroA-numeroB))
+alert ("a multiplicação" + (numeroA*numeroB))
+alert ("A divisão: "+ (numeroA/numeroB))
+
+// exercico 2
+alert ("Vamos calcular area de um quadrado")
+
+let base = prompt("Digite o tamanho da base")
+base = parseInt(base)
+
+let altura = prompt("Digite o tamanho da altura")
+altura = parseInt(altura)
+
+alert("A area do seu quadrado é: " + (base*altura))
